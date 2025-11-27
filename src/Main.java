ef591120af5e48ea155b2c45e8023f852a02b3f1
hyun-sai <202210985@sangmyung.kr>
2025-11-27 22:14:02 +0900

    java를 좀 건듬

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,37 @@
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.DriverManager;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+public static void main(String[] args) {
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+    Connection conn = null;
+    Statement stmt = null;
+    ResultSet rs = null;
+    try{
+
+        //Register JDBC driver
+        Class.forName("com.mysql.jdbc.Driver");
+
+        //Open a connection
+        System.out.println("Connecting to database...");
+        conn = DriverManager.getConnection("jdbc:mysql://localhost/mydb",
+                "root",'0211");
+
+        stmt = conn.createStatement();
+        rs=stmt.executeQuery("select * from movie")
+
+
+
+
+
+
+    }catch (SQLException ex) {
+        //Handle errors for JDBC
+        ex.printStackTrace();
+    }catch (Exception e){
+        //Handle errors for Class.forName
+        e.printStackTrace();
     }
 }
